aaa7df36aa24666227eedebab8f5619c01c967db
MahmoudElwan <mahmoudelwan460@gmail.com>
2020-06-02 22:33:38 +0200

    last check

--- Admindb.php
+++ Admindb.php
@@ -204,7 +204,7 @@ if(isset($_POST['deleteproduct']))
        <script src="js/jquery-3.3.1.min.js"></script>   
             <script src="js/popper.min.js"></script>    
             <script src="js/bootstrap.min.js"></script>
-     <script src="js/AdminDB11.js"></script>
+     <script src="js/AdminDB1.js"></script>
      </body>
 </html>
 
--- js/AdminDB1.js
+++ js/AdminDB1.js
@@ -11,7 +11,7 @@ let btn        =document.getElementById("myBtn");
 btn.onclick=function(){
     if(reg()==true)
         {
-                     alert("done");
+                    
         }
     
     else
@@ -25,6 +25,60 @@ let addPrice   =document.getElementById("addprice");
 function reg(){
     let regx1=/^[0-9\s]*$/;
     if(regx1.test(addPrice.value)==false)
+        {
+    
+          return false;   
+        }
+    else
+        {
+            return true;
+        }
+    
+}
+let btn1  =document.getElementById("myBtn2");
+btn1.onclick=function(){
+    if(reg2()==true)
+        {
+        }
+    
+    else
+        {
+        alert("Enter interger price only");
+            return false;
+
+        }
+}
+let updateprice   =document.getElementById("updateprice");
+function reg2(){
+    let regx1=/^[0-9\s]*$/;
+    if(regx1.test(updateprice.value)==false)
+        {
+    
+          return false;   
+        }
+    else
+        {
+            return true;
+        }
+    
+}
+let btn3  =document.getElementById("myBtn3");
+btn3.onclick=function(){
+    if(reg3()==true)
+        {
+        }
+    
+    else
+        {
+        alert("Enter string name  only");
+            return false;
+
+        }
+}
+let name   =document.getElementById("name");
+function reg3(){
+    let regx1=/^[a-zA-Z]/;
+    if(regx1.test(name.value)==false)
         {
     
           return false;   
